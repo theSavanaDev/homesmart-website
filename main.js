1c4bbf43c2378db7f67fe749da26e1d5e2069d49
Michael Owuor <michael.owuor@gmail.com>
2022-08-18 23:00:19 +0300

    feat: create navigation menu bar (#8)

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const navigationButton = document.querySelector('#menu');
+const menuBar = document.querySelector('[role="menubar"]');
+
+navigationButton.addEventListener('click', () => {
+	const isExpanded = JSON.parse(navigationButton.getAttribute('aria-expanded'));
+
+	navigationButton.setAttribute('aria-expanded', !isExpanded);
+	menuBar.classList.toggle('hidden');
+	menuBar.classList.toggle('flex');
+});
